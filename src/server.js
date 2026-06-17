fb446d088fe35caf601958ce0798593606975b54
Karan Agrahari <karanagraharee@gmail.com>
2026-06-17 18:54:20 +0530

    deployment error

--- src/server.js
+++ src/server.js
@@ -2,13 +2,12 @@ import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { env } from "./config/env.js";
 import { startReminderCron } from "./cron/reminderCron.js";
-
+import { Resend } from "resend";
 
 const bootstrap = async () => {
   try {
     await connectDB();
     startReminderCron();
-
     app.listen(env.port, () => {
       console.log(`Server running on port ${env.port}`);
     });
@@ -19,6 +18,3 @@ const bootstrap = async () => {
 };
 
 bootstrap();
-
-
-
